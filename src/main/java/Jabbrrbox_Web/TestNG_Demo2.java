e5186f4a2c3e9694b8ff8de1c0932e3bc8394c90
Anuj <anujsharma24k@gmail.com>
2026-05-28 18:25:01 +0530

    commented

--- src/main/java/Jabbrrbox_Web/TestNG_Demo2.java
+++ src/main/java/Jabbrrbox_Web/TestNG_Demo2.java
@@ -1,5 +1,6 @@
 package Jabbrrbox_Web;
 
+/*
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
@@ -7,7 +8,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
+*/
 public class TestNG_Demo2 {
 	
 	public class TestNG_Demo {
